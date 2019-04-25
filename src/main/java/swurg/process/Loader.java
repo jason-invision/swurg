f222b6d5629f3047b84c73aa8110b54bedec5126
Jason Rogers <jasonrogers@invisionapp.com>
2019-04-25 13:36:16 -0500

    Revert "Added Converter"
    This reverts commit 738f4ff03c57b36a2a49c534071c1593a82da69c.

--- src/main/java/swurg/process/Loader.java
+++ src/main/java/swurg/process/Loader.java
@@ -19,8 +19,6 @@ package swurg.process;
 import com.google.common.base.Strings;
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
-import io.swagger.v3.parser.converter.SwaggerConverter;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
@@ -55,8 +53,6 @@ public class Loader {
               resource));
     } else {
       validateSpecification(swagger, resource);
-      SwaggerConverter converter = new SwaggerConvert();
-      
       return swagger;
     }
   }
